7fc52563c44525d3c5dc3926b6e878b589f149a6
James A. Sral <jamessral@gmail.com>
2020-07-23 08:09:39 -0400

    Begin setting up basic infrustructure & read in file contents

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 pub fn tester() {
-	println!("Hello, world!");
+	let contents = reader("../data/hello_world.heg");
+
+	match contents {
+		Ok(c) => println!("{}", c),
+		_ => panic!("unable to read file")
+	}
+}
+
+fn reader(file_path: &str) -> std::io::Result<String> {
+	let file = File::open(file_path)?;
+	let mut buf_reader = BufReader::new(file);
+	let mut contents = String::new();
+	buf_reader.read_to_string(&mut contents)?;
+
+	Ok(contents)
 }
